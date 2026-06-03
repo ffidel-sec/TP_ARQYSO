820fbe73dd4705f736e0694b7e2369c178151ef9
ffidel <grandofidel7@gmail.com>
2026-06-03 20:13:14 -0300

    Agregar deploy con GitHub Actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeContext'
 import AnimatedRoutes from './components/AnimatedRoutes'
 
 export default function App() {
   return (
     <ThemeProvider>
-      <BrowserRouter>
+      <HashRouter>
         <AnimatedRoutes />
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   )
 }
